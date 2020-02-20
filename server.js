2e4c53bb6b712b432ae1545932373f955165ad09
felipehenrique159 <felipecod6@hotmail.com>
2020-02-20 15:56:44 -0300

    configuração do express e nunjucks

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static('public'))
+
 //config a template engine com o nunjucks
 
 const nunjucks = require("nunjucks")
@@ -9,9 +11,11 @@ nunjucks.configure("./", {
 })
 
 server.get("/", function (req, res) {
-    return res.render("index.html")
+    return res.render("index.html") //rederiza o index
 })
 
+
+//definir a porta do servidor
 server.listen(3000, function () {
     console.log("Iniciei o servidor!")
 });
