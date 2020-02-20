2f5bb9be5e4e0ae315195eb3af06270aa02999ef
felipehenrique159 <felipecod6@hotmail.com>
2020-02-20 15:41:54 -0300

    instalação do nunjucks

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const server = express()
 
+//config a template engine com o nunjucks
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server
+})
+
 server.get("/", function (req, res) {
-    return res.send("Ok cheguei ate aqui")
+    return res.render("index.html")
 })
 
 server.listen(3000, function () {
