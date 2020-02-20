1a065a1821982e35a3b219b192eeb24cb6894361
felipehenrique159 <felipecod6@hotmail.com>
2020-02-20 16:16:12 -0300

    trazendo dados para o template engine

--- server.js
+++ server.js
@@ -3,19 +3,47 @@ const server = express()
 
 server.use(express.static('public'))
 
-//config a template engine com o nunjucks
+//config template engine with nunjucks
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true
 })
+
+
+//list donors
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+
+    {
+        name: "Felipe Henrique",
+        blood: "A+"
+    },
+
+    {
+        name: "Izabelle Costa",
+        blood: "AB+"
+    },
+
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+
+]
 
 server.get("/", function (req, res) {
-    return res.render("index.html") //rederiza o index
+    return res.render("index.html",{donors}) //render index.html
 })
 
 
-//definir a porta do servidor
+
+
+//route port server
 server.listen(3000, function () {
     console.log("Iniciei o servidor!")
 });
